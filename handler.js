4002a8e86a59e603d331fa809f74e6a91498b71e
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-06-16 18:42:46 +0200

    Add files via upload

--- handler.js
+++ handler.js
@@ -1151,7 +1151,7 @@ let _user = global.db.data && global.db.data.users && global.db.data.users[m.sen
 
 const groupMetadata = (m.isGroup ? ((conn.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {}) || {}
 const participants = (m.isGroup ? groupMetadata.participants : []) || []
-let numBot = conn.user.lid.replace(/:.*/, '') || false
+let numBot = (conn.user.lid || '').replace(/:.*/, '') || false
 const detectwhat2 = m.sender.includes('@lid') ? `${numBot}@lid` : conn.user.jid
 const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.sender) : {}) || {}
 const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == detectwhat2) : {}) || {}
@@ -1297,7 +1297,7 @@ if (plugin.private && m.isGroup) { //Solo chat privado
 fail('private', m, this)
 continue
 }
-if (plugin.register == true && _user.registered == false) { // user registrado? 
+if (plugin.register == false && _user.registered == false) { // user registrado? 
 fail('unreg', m, this)
 continue
 }
